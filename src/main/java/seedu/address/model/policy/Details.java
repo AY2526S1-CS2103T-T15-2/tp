d407ab01566f232301cc81e2c0109afa032a1183
AndrescuIII-too <andrescuiii45@gmail.com>
2025-10-06 21:01:56 +0800

    Add validation for Policy details

--- src/main/java/seedu/address/model/policy/Details.java
+++ src/main/java/seedu/address/model/policy/Details.java
@@ -1,13 +1,23 @@
 package seedu.address.model.policy;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 public class Details {
+    public static final String MESSAGE_CONSTRAINTS = "Details should not be blank";
+
+    public static final String VALIDATION_REGEX = "\\S.*";
+
     private final String value;
 
     public Details(String details) {
         requireNonNull(details);
+        checkArgument(isValidDetails(details), MESSAGE_CONSTRAINTS);
         value = details;
+    }
+
+    public static boolean isValidDetails(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
