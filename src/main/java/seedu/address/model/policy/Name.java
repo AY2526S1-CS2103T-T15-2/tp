f82c2c1dd8bb3fda138b337da52845710b81725a
AndrescuIII-too <andrescuiii45@gmail.com>
2025-10-06 19:19:54 +0800

    Add validation for Policy name

--- src/main/java/seedu/address/model/policy/Name.java
+++ src/main/java/seedu/address/model/policy/Name.java
@@ -1,13 +1,24 @@
 package seedu.address.model.policy;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
 
 public class Name {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Names should only contain alphanumeric characters, spaces, and dashes, and it should not be blank";
+
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}-][\\p{Alnum}- ]*";
+
     private final String value;
 
     public Name(String name) {
         requireNonNull(name);
+        checkArgument(isValidName(name), MESSAGE_CONSTRAINTS);
         value = name;
+    }
+
+    public static boolean isValidName(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
