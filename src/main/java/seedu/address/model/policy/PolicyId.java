35e3ee28e9de28a93e279e27c593e993d19e4c19
AndrescuIII-too <andrescuiii45@gmail.com>
2025-10-08 15:42:52 +0800

    Refactor random string generation to util class

--- src/main/java/seedu/address/model/policy/PolicyId.java
+++ src/main/java/seedu/address/model/policy/PolicyId.java
@@ -2,15 +2,14 @@ package seedu.address.model.policy;
 
 import java.util.Random;
 
+import seedu.address.model.util.RandomUtil;
+
 /**
  * Represents a Policy's id that a user can reference.
  * Guarantees: immutable.
  */
 public class PolicyId {
 
-    private static final String ID_CHARACTERS = "abcdefghijklmnopqrstuvwxyz"
-            + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            + "0123456789";
     private static final int ID_LENGTH = 6;
 
     public final String value;
@@ -23,14 +22,7 @@ public class PolicyId {
      * Generates a random alphanumeric id to assign to a Policy.
      */
     public static PolicyId generate() {
-        Random random = new Random();
-        String id = random.ints(0, ID_CHARACTERS.length())
-                .map(ID_CHARACTERS::charAt)
-                .limit(ID_LENGTH)
-                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
-                .toString();
-
-        return new PolicyId(id);
+        return new PolicyId(RandomUtil.generateAlphanum(ID_LENGTH));
     }
 
     @Override
---
+++ src/main/java/seedu/address/model/util/RandomUtil.java
@@ -0,0 +1,22 @@
+package seedu.address.model.util;
+
+import java.util.Random;
+
+public class RandomUtil {
+
+    private static final String ALPHANUMERIC_CHARACTERS = "abcdefghijklmnopqrstuvwxyz"
+            + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            + "0123456789";
+
+    /**
+     * Generates a random alphanumeric string of given length.
+     */
+    public static String generateAlphanum(int length) {
+        Random random = new Random();
+        return random.ints(0, ALPHANUMERIC_CHARACTERS.length())
+                .map(ALPHANUMERIC_CHARACTERS::charAt)
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+    }
+}
