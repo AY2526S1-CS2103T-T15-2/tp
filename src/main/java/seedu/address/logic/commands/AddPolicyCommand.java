582825ae49db24807dfe7d0a645caa1a896cb4ee
KH-boop-bit <152586072+KH-boop-bit@users.noreply.github.com>
2025-10-09 19:59:15 +0800

    Code Quality Refactor

--- src/main/java/seedu/address/logic/commands/AddPolicyCommand.java
+++ src/main/java/seedu/address/logic/commands/AddPolicyCommand.java
@@ -2,11 +2,10 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DETAILS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-
 
 import seedu.address.commons.util.ToStringBuilder;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DETAILS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
