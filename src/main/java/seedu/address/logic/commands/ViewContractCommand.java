1d5a2f1f2a2e1ee7b3aff63e3bf77afb60aa0a90
Glendon Goh <167951234+Eggie23@users.noreply.github.com>
2025-10-09 22:01:07 +0800

    Update ViewContractCommand

--- src/main/java/seedu/address/logic/commands/ViewContractCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewContractCommand.java
@@ -1,24 +1,21 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.util.ToStringBuilder;
+
 import static java.util.Objects.requireNonNull;
 
 public class ViewContractCommand extends Command {
 
-    public static final String COMMAND_WORD = "view c";
+    public static final String COMMAND_WORD = "view c: -a";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Views a contract "
-            + "Parameters: " + "<Contract id>";
+    public static final String MESSAGE_SUCCESS = "Viewing all contracts ";
 
-    public static final String MESSAGE_SUCCESS = "Viewing contract: " + contract_id;
-
-    private final Contract_id toView; //id to be viewed
-
-    public ViewContractCommand(Contract_id id) {
-        requireNonNull(id);
-        toView = id;
-    }
 
     @Override
-    public Command
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        model.updateFilteredPolicyList(PREDICATE_SHOW_ALL_CONTRACTS)
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
 }
 
