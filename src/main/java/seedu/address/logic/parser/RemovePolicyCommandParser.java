dbfae9caced20f485df4a20270ea9a496ad280f0
KH-boop-bit <kenghong0202@gmail.com>
2025-10-15 01:20:42 +0800

    Add RemoveContract Prompt to RemovePolicy
    Add RemoveContract Prompt to RemovePolicyCommand
    - Add hasPolicyInList helper method
    - Add getPolicyInList helper method
    - Refactor execute method to include check for contracts
    
    Update RemovePolicyCommandParser to take in PREFIX_PID

--- src/main/java/seedu/address/logic/parser/RemovePolicyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/RemovePolicyCommandParser.java
@@ -1,6 +1,9 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PID;
+
+import java.util.stream.Stream;
 
 import seedu.address.logic.commands.RemovePolicyCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
@@ -17,13 +20,20 @@ public class RemovePolicyCommandParser implements Parser<RemovePolicyCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public RemovePolicyCommand parse(String args) throws ParseException {
-        try {
-            PolicyId id = ParserUtil.parsePolicyId(args);
-            return new RemovePolicyCommand(id);
-        } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemovePolicyCommand.MESSAGE_USAGE), pe);
+        ArgumentMultimap arguMultimap = ArgumentTokenizer.tokenize(args, PREFIX_PID);
+        if (!arePrefixesPresent(arguMultimap, PREFIX_PID) || !arguMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemovePolicyCommand.MESSAGE_USAGE));
         }
+        PolicyId id = ParserUtil.parsePolicyId(arguMultimap.getValue(PREFIX_PID).get());
+        return new RemovePolicyCommand(id);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
 }
