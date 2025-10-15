18ad9c5bfde7e1e41b09450708c4fb745a938048
KH-boop-bit <kenghong0202@gmail.com>
2025-10-15 13:26:02 +0800

    Remove Unused Import in RemovePolicyCommand

--- src/main/java/seedu/address/logic/commands/RemovePolicyCommand.java
+++ src/main/java/seedu/address/logic/commands/RemovePolicyCommand.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PID;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
