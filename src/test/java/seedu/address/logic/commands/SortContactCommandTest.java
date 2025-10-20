42e9639c622b9d8adada15be78e8818e822f17dd
AndrescuIII-too <andrescuiii45@gmail.com>
2025-10-20 21:32:53 +0800

    Add SortContactCommand execute unit tests

--- src/test/java/seedu/address/logic/commands/SortContactCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortContactCommandTest.java
@@ -5,15 +5,33 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
+import java.util.Comparator;
+
 import org.junit.jupiter.api.Test;
 
+import seedu.address.model.ModelStub;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.PersonComparatorType;
+import seedu.address.ui.ListPanelType;
 
 public class SortContactCommandTest {
 
     @Test
     public void constructor_nullComparatorType_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new SortContactCommand(null));
+    }
+
+    @Test
+    public void execute_personAcceptedByModel_addSuccessful() {
+        ModelStubAcceptingComparator modelStub = new ModelStubAcceptingComparator();
+
+        CommandResult commandResult = new SortContactCommand(PersonComparatorType.UNORDERED).execute(modelStub);
+        assertEquals(commandResult,
+                new CommandResult(SortContactCommand.MESSAGE_SUCCESS_UNORDERED, ListPanelType.CONTACT));
+
+        commandResult = new SortContactCommand(PersonComparatorType.ALPHABETICAL).execute(modelStub);
+        assertEquals(commandResult,
+                new CommandResult(SortContactCommand.MESSAGE_SUCCESS_ALPHABETICAL, ListPanelType.CONTACT));
     }
 
     @Test
@@ -44,5 +62,15 @@ public class SortContactCommandTest {
         SortContactCommand sortContactCommand = new SortContactCommand(comparatorType);
         String expected = SortContactCommand.class.getCanonicalName() + "{comparatorType=" + comparatorType + "}";
         assertEquals(expected, sortContactCommand.toString());
+    }
+
+    /**
+     * A Model stub that always accept the person being added.
+     */
+    private class ModelStubAcceptingComparator extends ModelStub {
+        @Override
+        public void sortPersons(Comparator<Person> comparator) {
+
+        }
     }
 }
