ce2cc4f0e187469a54ef633b77321f1fb2a0c243
Joshua-Seah <194608515+Joshua-Seah@users.noreply.github.com>
2025-10-22 22:30:10 +0800

    Fix minor grammatical error in javadoc comment

--- src/main/java/seedu/address/logic/parser/EditContractCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditContractCommandParser.java
@@ -22,7 +22,7 @@ public class EditContractCommandParser implements Parser<EditContractCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the EditContractCommand
      * and returns an EditContractCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform to the expected format
      */
     public EditContractCommand parse(String args) throws ParseException {
         requireNonNull(args);
