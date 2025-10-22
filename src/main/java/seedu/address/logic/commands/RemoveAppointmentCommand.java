b08b2217a59921a87b7440c87051e8ffff61c008
Damien Lim <damienlim@Damiens-MacBook-Pro-3.local>
2025-10-22 11:04:57 +0800

    add RemoveAppointmentCommand class

---
+++ src/main/java/seedu/address/logic/commands/RemoveAppointmentCommand.java
@@ -0,0 +1,75 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.Messages;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.appointment.AppointmentId;
+import seedu.address.ui.ListPanelType;
+
+/**
+ * Removes an appointment from iCon.
+ */
+public class RemoveAppointmentCommand extends Command {
+
+    public static final String COMMAND_WORD = "remove_appointment";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Removes an appointment from iCon. "
+            + "Parameters: "
+            + "Appointment ID (must be a positive integer)\n"
+            + "Example: " + COMMAND_WORD + " gVVGzn";
+
+    public static final String MESSAGE_REMOVE_APPOINTMENT_SUCCESS = "Removed Contract: %1$s";
+
+    private final AppointmentId aId;
+
+    public RemoveAppointmentCommand(AppointmentId targetIndex) {
+        this.aId = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Appointment> lastShownList = model.getFilteredAppointmentList();
+
+        if (lastShownList.stream().anyMatch(x -> x.getAId().equals(aId))) {
+            Appointment appointmentToRemove = lastShownList.stream()
+                    .filter(x -> x.getAId().equals(aId))
+                    .findFirst()
+                    .get();
+            model.removeAppointment(appointmentToRemove);
+            return new CommandResult(String.format(MESSAGE_REMOVE_APPOINTMENT_SUCCESS, appointmentToRemove.getAId()),
+                    ListPanelType.APPOINTMENT);
+        } else {
+            throw new CommandException(Messages.MESSAGE_APPOINTMENT_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof RemoveAppointmentCommand)) {
+            return false;
+        }
+
+        RemoveAppointmentCommand otherRemoveAppointment = (RemoveAppointmentCommand) other;
+        return aId.equals(otherRemoveAppointment.aId);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("aId", aId)
+                .toString();
+    }
+}
