788571d8028fd9fe6c0e44a2b8fb1026bddd9e7b
AndrescuIII-too <andrescuiii45@gmail.com>
2025-10-23 20:54:04 +0800

    Remove unused import

--- src/test/java/seedu/address/logic/commands/ViewContactCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewContactCommandTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalData.CARL;
 import static seedu.address.testutil.TypicalData.ELLE;
 import static seedu.address.testutil.TypicalData.FIONA;
 import static seedu.address.testutil.TypicalData.getTypicalAddressBook;
